a784efbea106e04c045968b6b6e846f10ecf6a35
Tabalamari <tabalamari@gmail.com>
2023-09-11 11:30:13 +0300

    grid added

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,7 @@
 async function fetchData() {
     try {
         const data = await fetch("https://voodoo-sandbox.myshopify.com/products.json?limit=24");
+        console.log(data)
         const result = await data.json();
         return result;
     }
@@ -24,24 +25,26 @@ function getProductElement(product) {
 
 
     const divNewProduct = document.createElement('div');
-    // divNewProduct.classList.add("w-72",)
+    divNewProduct.classList.add("w-full", "text-center")
 
 
     const divUsed = document.createElement("div");
     divNewProduct.appendChild(divUsed);
-    divUsed.classList.add("w-64", "h-72", "border-2", "border-black", "rounded")
+    divUsed.classList.add("border-2", "border-black", "rounded");
 
 
     const imgProduct = document.createElement("img");
-    if (product.images.lenght > 0) {
+    console.log(product)
+    if (product.images.length > 0) {
         imgProduct.src = product.images[0].src
     }
     divUsed.appendChild(imgProduct);
+    imgProduct.classList.add("relative");
 
 
-    const pUsed = document.createElement("p");
+    const pUsed = document.createElement("div");
     divUsed.appendChild(pUsed);
-    pUsed.classList.add("bg-black", "text-white", "w-10", "text-xs", "rounded", "mt-2", "ml-2", "text-center")
+    pUsed.classList.add("bg-black", "text-white", "w-10", "h-6", "absolute", "text-xs", "rounded", "mt-2", "ml-2");
     pUsed.innerText = "USED";
 
 
@@ -55,7 +58,7 @@ function getProductElement(product) {
 
     const pName = document.createElement("p");
     divNameAndCondition.appendChild(pName);
-    pName.classList.add("font-bold","w-12", "overflow-hidden", "whitespace-nowrap", "text-ellipsis");
+    pName.classList.add("font-bold", "w-12", "overflow-hidden", "whitespace-nowrap", "text-ellipsis");
     pName.innerText = product.title;
 
     const pCondition = document.createElement("p");
@@ -68,7 +71,7 @@ function getProductElement(product) {
 
     const pProductName = document.createElement("p");
     divProductNameAndCondition.appendChild(pProductName);
-    pProductName.classList.add("font-bold")
+    pProductName.classList.add("font-bold");
     pProductName.innerText = "000 KR.";
 
     const pProductCondition = document.createElement("p");
@@ -79,7 +82,7 @@ function getProductElement(product) {
 
     const buttonAddToCard = document.createElement("button");
     divNewProduct.appendChild(buttonAddToCard);
-    buttonAddToCard.classList.add("bg-black", "text-white", "h-10", "rounded", "font-sans", "text-sm", "mb-10")
+    buttonAddToCard.classList.add("bg-black", "text-white", "h-10", "rounded", "font-sans", "text-sm", "mb-10", "w-full");
     buttonAddToCard.innerText = "ADD TO CARD";
 
     return divNewProduct;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html, js}"],
+  content: ["./dist/**/*.{html, js}"],
   theme: {
     extend: {
-        width: {
-            '101': '101px',
-        }
     },
     colors: {
         black: '#000000',
